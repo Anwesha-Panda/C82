22ebec46df0872bcaa8961ace32df6f849e3452c
Anwesha-Panda <97949949+Anwesha-Panda@users.noreply.github.com>
2022-04-26 16:29:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{
+    colour=document.getElementById("color").value;
+    width_of_line=document.getElementById("width_of_line").value;
+    radius=document.getElementById("radius").value;
+    mouseEvent="mouseDown";
+}
+function my_mousemove(e)
+{
+    
+    current_position_of_mouse_x=e.client-canvas.offsetLeft;
+    current_position_of_mouse_y=e.client-canvas.offsetTop;
+    if(mouseEvent=="mousedown"){
+        console.log("Current position of x and y coordinates=");
+        console.log("x = " + current_position_of_mouse_x+"y="+current_position_of_mouse_y);
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=width_of_line;
+        ctx.arc(current_position_of_mouse_x,current_position_of_mouse_y,radius,0,2*Math.PI);
+        ctx.stroke();
+    }
+
+}
